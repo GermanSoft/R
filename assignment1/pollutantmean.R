ead393a74b77d847235062261800f69e9aaf59d9
GermanSoft <martin78@topmail-files.de>
2015-02-08 00:59:24 +0100

    added pollutanmean.r

--- assignment1/pollutantmean.R
+++ assignment1/pollutantmean.R
@@ -9,6 +9,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
+  filename<-sprintf("%s\\%03d.csv",directory,id)
+  total<-data.frame()
+  for(file in filename)
+  {  
+    data<-read.csv(file)
+    total<-rbind(total,data)      
+  }
+  
+  mean(total[,pollutant],na.rm = TRUE) 
+  
+  
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
 }
